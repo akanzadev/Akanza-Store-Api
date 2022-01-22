b95c498b285a974b1680bb174411cf7190143d78
Luis Mamani Lingan <72869012@certus.edu.pe>
2022-01-22 14:20:48 -0500

    fix error in migrations

--- db/migrations/20220109191706-create-customers.js
+++ db/migrations/20220109191706-create-customers.js
@@ -1,10 +1,48 @@
 'use strict'
-
-const { CustomerSchema, CUSTOMER_TABLE } = require('../models/customer.model')
+const { DataTypes } = require('sequelize')
+const { USER_TABLE } = require('../models/user.model')
+const { CUSTOMER_TABLE } = require('../models/customer.model')
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable(CUSTOMER_TABLE, CustomerSchema)
+    await queryInterface.createTable(CUSTOMER_TABLE, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: DataTypes.STRING
+      },
+      lastName: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        field: 'last_name'
+      },
+      phone: {
+        allowNull: true,
+        type: DataTypes.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: 'created_at',
+        defaultValue: Sequelize.NOW
+      },
+      userId: {
+        field: 'user_id',
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: USER_TABLE,
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL'
+      }
+    })
   },
 
   down: async (queryInterface, Sequelize) => {
