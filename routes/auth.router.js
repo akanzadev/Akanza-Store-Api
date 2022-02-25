94a525167fe51f9e415d49e8c0d56fecc1858bb6
Luis Mamani Lingan <72869012@certus.edu.pe>
2022-02-25 10:06:29 -0500

    validate token endpoint

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -26,39 +26,49 @@ router.post(
   }
 )
 
-// POST /api/v1/auth/recovery
-router.post(
-  '/recovery',
+// POST /api/v1/auth/validate-token
+router.get(
+  '/validate-token',
+  [
+    passport.authenticate('jwt', { session: false })],
   async (req, res, next) => {
     try {
-      const { email } = req.body
-      const rta = await service.sendRecovery(email)
-      res.status(200).json(rta)
+      const { user } = req
+      console.log(user)
+      const rta = service.generateJWT(user)
+      res.status(201).json(rta)
     } catch (error) {
       next(error)
     }
   }
 )
 
-// POST /api/v1/auth/change-password
-router.post('/change-password',
-  async (req, res, next) => {
-    try {
-      const { token, newPassword } = req.body
-      const rta = await service.changePassword(token, newPassword)
-      res.json(rta)
-    } catch (error) {
-      next(error)
-    }
+// POST /api/v1/auth/recovery
+router.post('/recovery', async (req, res, next) => {
+  try {
+    const { email } = req.body
+    const rta = await service.sendRecovery(email)
+    res.status(200).json(rta)
+  } catch (error) {
+    next(error)
   }
-)
+})
+
+// POST /api/v1/auth/change-password
+router.post('/change-password', async (req, res, next) => {
+  try {
+    const { token, newPassword } = req.body
+    const rta = await service.changePassword(token, newPassword)
+    res.json(rta)
+  } catch (error) {
+    next(error)
+  }
+})
 
 // POST /api/v1/auth/profile
 router.get(
   '/profile',
-  [
-    passport.authenticate('jwt', { session: false })
-  ],
+  [passport.authenticate('jwt', { session: false })],
   async (req, res, next) => {
     try {
       const { user } = req
